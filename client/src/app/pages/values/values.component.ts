45e372bdef97680dc3568a3825f2288dc1af6929
Andy Cai <andy.cai258@gmail.com>
2022-05-02 02:52:39 -0700

    Changed scroll bar appearance

--- client/src/app/pages/values/values.component.ts
+++ client/src/app/pages/values/values.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./values.component.css']
 })
 export class ValuesComponent implements OnInit {
-
-
   index: number = 1;
 
   values: string[] = [
